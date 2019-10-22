2b2c154bec88386e5a4e162fd5e190fe73f47cfa
kimseohae <seohae0406@gmail.com>
2019-10-22 21:35:21 +0900

    seohae_ user jpa crud test

--- src/main/java/com/wds/server/wdsserver/domain/User.java
+++ src/main/java/com/wds/server/wdsserver/domain/User.java
@@ -1,8 +1,12 @@
 package com.wds.server.wdsserver.domain;
 
+import io.swagger.annotations.ApiParam;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -17,58 +21,70 @@ public class User {
     @Id
     @Column(name = "idx")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiParam(hidden = true)
     private Long idx;
 
     @Column(name = "_id")
+    @ApiParam(defaultValue = "test2019")
     private String id;
 
     @Column(name = "pass", nullable = false)
     @NotNull
     @Length(max = 255)
-    private String pass ;
+    @ApiParam(defaultValue = "test")
+    private String pass;
 
     @Column(name = "name", nullable = false)
     @NotNull
     @Length(max = 255)
-    private String name  ;
+    @ApiParam(defaultValue = "김터디")
+    private String name;
 
     @Column(name = "nick_name", nullable = false)
     @NotNull
     @Length(max = 255)
+    @ApiParam(defaultValue = "컴맨")
     private String nick_name;
 
     @Column(name = "address", nullable = false)
     @NotNull
     @Length(max = 255)
+    @ApiParam(defaultValue = "서울")
     private String address;
 
-    @Column(name = "photo", nullable = false)
-    @NotNull
+    @Column(name = "photo")
     @Length(max = 255)
     private String photo;
 
     @Column(name = "tel", nullable = false)
     @NotNull
+    @ApiParam(defaultValue = "01012341234")
     private String tel;
 
-    @Column(name = "token", nullable = false)
-    @NotNull
+    @Column(name = "token")
+    @ApiParam(hidden = true)
     private String token;
 
     @Column(name = "del_yn")
+    @ApiParam(hidden = true)
     private String del_yn ;
 
     @Column(name = "create_date")
-    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @ApiParam(hidden = true)
     private Date create_date;
 
     @Column(name = "modify_date")
+    @UpdateTimestamp
+    @ApiParam(hidden = true)
     private Date modify_date;
 
     @Column(name = "expired_date")
+    @ApiParam(hidden = true)
     private Date expired_date;
 
     @Column(name = "logout_date")
+    @ApiParam(hidden = true)
     private Date logout_date;
 
 
