eaabdfb0c8e01939da1a7eee117db9d07a8fb7f1
JIKYUNGHEE <wlrudgml517@gmail.com>
2019-11-16 09:28:25 +0900

    junit Controller 테스트 코드 추가

---
+++ src/test/java/com/wds/server/wdsserver/controller/UserControllerTest.java
@@ -0,0 +1,123 @@
+package com.wds.server.wdsserver.controller;
+
+import static org.mockito.Mockito.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wds.server.wdsserver.commons.ResponseType;
+import com.wds.server.wdsserver.domain.User;
+import com.wds.server.wdsserver.service.UserService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestClientTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserControllerTest {
+    private final static String BASE_URI = "/api/v1/users";
+
+    @Mock
+    private UserService mockUserService;
+    @Mock
+    private ResponseType mockResponseType;
+    private MockMvc mockMvc;
+
+    @Mock
+    private User mockUser;
+
+    @InjectMocks
+    private UserController mockUserController;
+
+    @BeforeAll
+    public void init(){
+        mockUserController = new UserController(mockUserService, mockResponseType);
+        mockMvc = MockMvcBuilders.standaloneSetup(mockUserController).build();
+    }
+
+    @Test
+    public void whenRequestGETBaseURI_thenGetOKStatus() throws Exception {
+        List<User> mockUserList = new ArrayList<>(10);
+
+        int idx = 1;
+        for(User mockUser : mockUserList){
+            mockUser = new User();
+            mockUser.setIdx(Integer.toUnsignedLong(idx));
+            mockUser.setId("cId"+idx);
+            mockUser.setName("cName"+idx);
+            mockUser.setPass("cPass"+idx);
+
+            idx++;
+        }
+
+        when(mockUserService.getAllUsers()).thenReturn(mockUserList);
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URI))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    public void whenRequestPOSTBaseURI_thenGetOKStatus() throws Exception { // TODO 400 error
+        int idx = 1;
+        mockUser = new User();
+        mockUser.setIdx(Integer.toUnsignedLong(idx));
+        mockUser.setId("JKH");
+        mockUser.setPass("1234");
+        mockUser.setName("지경희");
+        mockUser.setNick_name("갱갱");
+        mockUser.setAddress("서울특별시");
+        mockUser.setTel("010-5175-0553");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String mockUserJson = mapper.writeValueAsString(mockUser);
+
+        when(mockUserService.addUser(Mockito.any(User.class))).thenReturn(mockUser);
+        mockMvc.perform(MockMvcRequestBuilders.post(BASE_URI)
+                        .content(mockUserJson)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    public void whenGetUserIdx1_thenGetOKStatus() throws Exception {
+        mockUser = new User();
+        int idx = 1;
+        mockUser.setIdx(Integer.toUnsignedLong(idx));
+        mockUser.setId("JKH");
+        mockUser.setName("지경희");
+        mockUser.setPass("1234");
+
+        when(mockUserService.getUser(Mockito.anyLong())).thenReturn(mockUser);
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URI+"/"+idx))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(200));
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("지경희"));
+//                .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    public void whenDeleteUserIdx1_thenGetOKStatus() throws Exception{
+        int idx = 1;
+
+        doNothing().when(mockUserService).deleteUser(Mockito.anyLong());
+        mockMvc.perform(MockMvcRequestBuilders.delete(BASE_URI+"/"+idx))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+
+}
