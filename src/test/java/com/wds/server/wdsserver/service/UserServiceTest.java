e49cb5ed91cf10103b0b64e94b19a0016c63b9c8
이재두 <dlwoen9@godo.co.kr>
2019-11-22 01:03:35 +0900

    junit5 버전 변경 테스트코드 추가

--- src/test/java/com/wds/server/wdsserver/service/UserServiceTest.java
+++ src/test/java/com/wds/server/wdsserver/service/UserServiceTest.java
@@ -1,22 +1,22 @@
 package com.wds.server.wdsserver.service;
 
-import com.wds.server.wdsserver.WdsServerApplication;
 import com.wds.server.wdsserver.domain.User;
 import com.wds.server.wdsserver.repository.UserRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-//@ExtendWith(SpringExtension.class)
-@RestClientTest
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
 
@@ -29,28 +29,53 @@ public class UserServiceTest {
     @Mock
     private User mockUser;
 
+    private List<User> userList = new ArrayList<>();
+    private int _userCount = 3;
+
     @BeforeAll
     public void setUp() throws Exception {
         mockUser = new User();
-
         mockUser.setIdx(1L);
+        mockUser.setPass("1234");
+        mockUser.setNick_name("별");
         mockUser.setName("이재두");
-
-        when(mockUserRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(mockUser));
-
+        mockUser.setAddress("서울");
+        mockUser.setTel("01012344321");
     }
 
     @Test
+    @Order(1)
     public void callAllUserOrderByCreatedDate() throws Exception {
         mockUserService.getAllUsersOrderByCreatedDate();
         verify(mockUserRepository, times(1)).getAllUsersOrderByCreatedDate();
     }
 
     @Test
-    public void whenUserIdxIs1_thenName이재두() throws Exception {
-        User userInfo = mockUserService.getUser((long) 1);
-        verify(mockUserRepository, times(1)).findById(1L);
-        assertEquals("이재두", userInfo.getName());
+    @Order(3)
+    public void whenUserIdxIsIdx_thenName이재두Idx() throws Exception {
+        for(int i = 1; i <= _userCount; i++) {
+            mockUser = new User();
+            mockUser.setIdx((long) i);
+            mockUser.setPass("1234");
+            mockUser.setNick_name("별");
+            mockUser.setName("이재두" + i);
+            mockUser.setAddress("서울");
+            mockUser.setTel("01012344321");
+            userList.add(mockUser);
+
+            when(mockUserRepository.findById((long) i)).thenReturn(Optional.ofNullable(userList.get(i - 1)));
+        }
+        for(int idx = 1; idx <= _userCount; idx++) {
+            Optional<User> mockUserList = mockUserRepository.findById((long) idx);
+            assertEquals("이재두" + idx, mockUserList.get().getName());
+        }
+    }
+
+    @Test
+    @Order(2)
+    public void whenUserIdxIsNull_thenNameNull() throws Exception {
+        when(mockUserRepository.findById(2L)).thenReturn(null);
+        assertEquals(null, mockUserRepository.findById(2L));
     }
 
 }
