964d261ea890685d975a5f669b19e42909a0128d
이재두 <dlwoen9@godo.co.kr>
2019-11-26 22:55:51 +0900

    querydsl JpaQueryFactory 객체 설정

--- src/main/java/com/wds/server/wdsserver/repository/UserRepositoryImpl.java
+++ src/main/java/com/wds/server/wdsserver/repository/UserRepositoryImpl.java
@@ -1,31 +1,32 @@
 package com.wds.server.wdsserver.repository;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.wds.server.wdsserver.domain.QUser;
 import com.wds.server.wdsserver.domain.User;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 
 @Repository
 public class UserRepositoryImpl extends QuerydslRepositorySupport implements UserRepositoryCustom {
 
-    private JPAQueryFactory queryFactory = new JPAQueryFactory(this.getEntityManager());;
+    private final JPAQueryFactory jpaQueryFactory;
     private QUser quser = QUser.user;
 
-    public UserRepositoryImpl() { super(User.class);}
+    public UserRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
+        super(User.class);
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
 
     @Override
     public List<User> getAllUsersOrderByCreatedDate() {
-        return queryFactory.selectFrom(quser).orderBy(quser.create_date.desc()).fetch();
+        return jpaQueryFactory.selectFrom(quser).orderBy(quser.create_date.desc()).fetch();
     }
 
     @Override
     public List<User> lookUpUsers(String filter, String query) {    // TODO filter
-        return queryFactory.selectFrom(quser)
+        return jpaQueryFactory.selectFrom(quser)
                 .where(quser.name.like(query).or(quser.tel.like(query)))
                 .fetch();
     }
